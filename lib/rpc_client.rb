7696aaec0d302b517551ec0ef1791b69a0d4d657
vincent <vincent4502237@gmail.com>
2019-02-28 11:30:10 +0800

    添加一些辅助方法

--- lib/rpc_client.rb
+++ lib/rpc_client.rb
@@ -17,7 +17,65 @@ class RPCClient
 
   end
 
+  def default_params
+    default_params = {
+      :Format           => 'JSON',
+      :SignatureMethod  => 'HMAC-SHA1',
+      :SignatureNonce   => SecureRandom.hex(16),
+      :SignatureVersion => '1.0',
+      :Timestamp        => Time.now.strftime('%Y-%m-%dT%H:%M:%SZ'),
+      :AccessKeyId      => self.access_key_id,
+      :Version          => self.api_version,
+    }
+    default_params.merge!(SecurityToken: self.security_token) if self.security_token
+    default_params
+  end
+
   private
+
+  def encode(string)
+    CGI.escape string
+  end
+
+  def format_params(param_hash)
+    param_hash.keys.each { |key| param_hash[key.to_s.upcase_first.to_sym] = param_hash.delete key }
+    param_hash
+  end
+
+  def replace_repeat_list(target, key, repeat)
+    repeat.each_with_index do |item, index|
+      if item && item.instance_of?(Hash)
+        item.each_key { |k| target["#{key}.#{index.next}.#{k}"] = item[k] }
+      else
+        target["#{key}.#{index.next}"] = item
+      end
+    end
+    target
+  end
+
+  def flat_params(params)
+    target = {}
+    params.each do |key, value|
+      if value.instance_of?(Array)
+        replace_repeat_list(target, key, value)
+      else
+        target[key] = value
+      end
+    end
+    target
+  end
+
+  def normalize(params)
+    flat_params(params)
+      .stringify_keys
+      .sort
+      .to_h
+      .map { |key, value| [encode(key), encode(value)] }
+  end
+
+  def canonicalize(normalized)
+    URI.encode_www_form normalized
+  end
 
   def validate(config)
     raise ArgumentError, 'must pass "config"' unless config
--- spec/rpc_client_spec.rb
+++ spec/rpc_client_spec.rb
@@ -73,4 +73,85 @@ describe 'rpc core' do
     end
   end
 
+  describe 'default params' do
+
+    it 'should ok' do
+      rpc_client          = RPCClient.new(
+        endpoint:          'https://ecs.aliyuncs.com/',
+        api_version:       '1.0',
+        access_key_id:     'access_key_id',
+        access_key_secret: 'access_key_secret',
+      )
+      default_params_keys = %w(Format SignatureMethod SignatureNonce SignatureVersion Timestamp AccessKeyId Version)
+      expect(rpc_client.default_params.stringify_keys.keys).to match_array default_params_keys
+    end
+  end
+
+  it 'should ok with securityToken' do
+    rpc_client          = RPCClient.new(
+      endpoint:          'https://ecs.aliyuncs.com/',
+      api_version:       '1.0',
+      access_key_id:     'access_key_id',
+      access_key_secret: 'access_key_secret',
+      security_token:    'security_token'
+    )
+    default_params_keys = %w(Format SignatureMethod SignatureNonce SignatureVersion Timestamp AccessKeyId Version SecurityToken)
+    expect(rpc_client.default_params.stringify_keys.keys).to match_array default_params_keys
+  end
+
+  describe 'RPC private methods' do
+
+    let(:rpc_client) do
+      RPCClient.new(
+        endpoint:          'https://ecs.aliyuncs.com/',
+        api_version:       '1.0',
+        access_key_id:     'access_key_id',
+        access_key_secret: 'access_key_secret',
+        security_token:    'security_token'
+      )
+    end
+
+    it 'formatParams should ok' do
+      expect(rpc_client.send(:format_params, { foo: 1, bar: 2 })).to eq(Foo: 1, Bar: 2)
+    end
+
+    it 'encode should ok' do
+      expect(rpc_client.send(:encode, 'str')).to eq 'str'
+      expect(rpc_client.send(:encode, 'str\'str')).to eq 'str%27str'
+      expect(rpc_client.send(:encode, 'str(str')).to eq 'str%28str'
+      expect(rpc_client.send(:encode, 'str)str')).to eq 'str%29str'
+      expect(rpc_client.send(:encode, 'str*str')).to eq 'str%2Astr'
+    end
+
+    it 'replace_repeat_list should ok' do
+      expect(rpc_client.send(:replace_repeat_list, {}, 'key', [])).to eq({})
+      expect(rpc_client.send(:replace_repeat_list, {}, 'key', ['value'])).to eq({ 'key.1' => 'value' })
+      expect(rpc_client.send(:replace_repeat_list, {}, 'key', [{ :Domain => '1.com' }])).to eq({ 'key.1.Domain' => '1.com' })
+    end
+
+    it 'flat_params should ok' do
+      expect(rpc_client.send(:flat_params, {})).to eq({})
+      expect(rpc_client.send(:flat_params, { key: ['value'] })).to eq({ 'key.1' => 'value' })
+      expect(rpc_client.send(:flat_params, { key: 'value' })).to eq({ key: 'value' })
+      expect(rpc_client.send(:flat_params, { key: [{ Domain: '1.com' }] })).to eq({ 'key.1.Domain' => '1.com' })
+    end
+
+    it 'normalize should ok' do
+      expect(rpc_client.send(:normalize, {})).to be_empty
+      expect(rpc_client.send(:normalize, { key: ['value'] })).to match_array [%w(key.1 value)]
+      expect(rpc_client.send(:normalize, { key: 'value' })).to match_array [%w(key value)]
+      expect(rpc_client.send(:normalize, { key: [{ Domain: '1.com' }] })).to match_array [%w(key.1.Domain 1.com)]
+      expect(rpc_client.send(:normalize, { a: 'value', c: 'value', b: 'value' }))
+        .to match_array [%w(a value), %w(b value), %w(c value)]
+    end
+
+    it 'canonicalize should ok' do
+      expect(rpc_client.send(:canonicalize, [])).to be_empty
+      expect(rpc_client.send(:canonicalize, { foo: 1 })).to eq 'foo=1'
+      expect(rpc_client.send(:canonicalize, [['foo', 1]])).to eq 'foo=1'
+      expect(rpc_client.send(:canonicalize, { foo: 1, bar: 2 })).to eq 'foo=1&bar=2'
+      expect(rpc_client.send(:canonicalize, [['foo', 1], ['bar', 2]])).to eq 'foo=1&bar=2'
+    end
+  end
+
 end
