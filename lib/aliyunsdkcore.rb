500491d507c6f01aa9f8f5286ae830c21b318724
Jackson Tian <shyvo1987@gmail.com>
2019-11-13 16:44:02 +0800

    Release v0.0.11

--- lib/aliyunsdkcore.rb
+++ lib/aliyunsdkcore.rb
@@ -2,7 +2,7 @@ require 'aliyunsdkcore/rpc_client'
 require 'aliyunsdkcore/roa_client'
 
 module AliyunSDKCore
-  VERSION = "0.0.10"
+  VERSION = "0.0.11"
   DEFAULT_UA = "AlibabaCloud (#{Gem::Platform.local.os}; " +
     "#{Gem::Platform.local.cpu}) Ruby/#{RUBY_VERSION} Core/#{VERSION}"
 end
